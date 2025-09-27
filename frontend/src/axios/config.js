a9086ca1fae1aac488bfbadebff6130326d7b413
Thiago Andrade <thiagopollinho@gmail.com>
2025-09-26 23:24:16 -0300

    adding backend deploy url

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const fetchApi = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "https://edu-system-my2r.onrender.com/api/",
     headers: {
         "Content-Type": "application/json",
     },
